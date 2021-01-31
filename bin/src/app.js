bc0dbfbbea8e03a3b47f1b23bdd54d27ef0b9838
Johannes Franzen <johannes-franzen@t-online.de>
2021-01-31 23:30:05 +0100

    refactor, added compiled ts to .gitignore

--- bin/src/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var main_1 = __importDefault(require("./main"));
-var electron_1 = require("electron");
-main_1.default.main(electron_1.app, electron_1.BrowserWindow);
-//# sourceMappingURL=app.js.map
